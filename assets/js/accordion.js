34202f4e76897113b5ecec39229184a4de5eca87
Angelo Spampinato <spampinatodesign@gmail.com>
2019-10-26 17:23:23 -0400

    adding accordion

---
+++ assets/js/accordion.js
@@ -0,0 +1,10 @@
+const storeAccordions = document.querySelectorAll(".accordion")
+const storeAccordionPanels = document.querySelectorAll(".accordion-panel")
+
+for(let storeAccordion of storeAccordions) {
+    storeAccordion.addEventListener('click', () => {
+        for(let storeAccordionPanel of storeAccordionPanels) {
+            storeAccordionPanel.styl
+        }
+    })
+}
